bdf6964b86413d8b7a6533487350348f5901ecf8
Safouane <safouane@Safouanes-MacBook-Air.local>
2020-12-21 22:12:14 +0100

    see several characters now

--- src/App.js
+++ src/App.js
@@ -1,50 +1,46 @@
 import logo from './logo.svg';
 import React, {useState, useEffect} from 'react';
 import './App.css';
-import {Card} from "./Components/Card";
+import Card from "./Components/Card";
 import {Jumbotron} from "./Components/Jumbotron";
 import {Navbar} from "./Components/Navbar";
 import {Row} from "./Components/Row";
 import axios from 'axios';
 
 function App() {
-    const url = 'https://rickandmortyapi.com/api/character/1';
+    const url = 'https://rickandmortyapi.com/api/character/';
     const [character, setCharacter] = useState(null)
 
     useEffect(() => {
         axios.get(url).then(response => {
-            setCharacter(response.data)
+            setCharacter(response.data.results)
         })
     },[url])
 
+    if(character) {
+        return (
 
-if (character){
-    return (
-      <div className="main">
-          <Navbar></Navbar>
-          <Jumbotron></Jumbotron>
-<div className="container">
-    <div className="row mt-5 justify-content-center">
-
-    <Card
-        image={character.image}
-        name={character.name}
-        species={character.species}
-        status={character.status}
-    location={character.location.name}>
-
-    </Card>
-
-    <Card>
-    </Card>
-</div>
+            <div className="main">
+                <Navbar></Navbar>
+                <Jumbotron></Jumbotron>
+                <div className="container">
+                    <div className="row mt-5 justify-content-center">
+                        {character.map(char => {
+                            return <Card
+                                image={char.image}
+                                species={char.species}
+                                status={char.status}
+                                location={char.location.name}
+                                name={char.name}  />;
+                        })}
+                    </div>
 
 
 
-</div>
-      </div>
-  );
-}
+                </div>
+            </div>
+        );
+    }
     return (
         <div className="main">
             <Navbar></Navbar>
@@ -55,22 +51,15 @@ if (character){
                     <Card>
 
                     </Card>
+                    <Card>
 
-                    <Card
-                        name="Safouane"
-                        text="Something here is some cool text">
                     </Card>
                 </div>
-
-
-
             </div>
         </div>
     );
 
 }
-
-
 
 
 export default App;
--- src/Components/Card.js
+++ src/Components/Card.js
@@ -2,23 +2,23 @@ import React from 'react';
 import './Card.css'
 
 
-export class Card extends React.Component {
+export default function Card(props) {
 
-    render() {
 
-        return (
 
-            <div className="card card-width">
-                <div className="card-body">
-                    <img className="card-img-top" src={this.props.image} alt=""/>
-                    <h5 className="card-title" >Name: {this.props.name}</h5>
-                    <p className="card-text" >Species: {this.props.species}</p>
-                    <p className="card-text" >Status: {this.props.status}</p>
-                    <p className="card-text" >Location: {this.props.location}</p>
-                    <a href="#" className="btn btn-primary">Go somewhere</a>
-                </div>
-                </div>
-        );
-    }
+    return (
+
+        <div className="card card-width">
+            <div className="card-body">
+                <img className="card-img-top" src={props.image} alt=""/>
+                <h5 className="card-title" >Name: {props.name}</h5>
+                <p className="card-text" >Species: {props.species}</p>
+                <p className="card-text" >Status: {props.status}</p>
+                <p className="card-text" >Location: {props.location}</p>
+                <a href="#" className="btn btn-primary">Go somewhere</a>
+            </div>
+        </div>
+    );
+
 
 }
