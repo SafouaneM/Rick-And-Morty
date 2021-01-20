7046d78acdc94412b697aa2f3f305cf1c463e732
patrick <patrick.laranjo@gmail.com>
2021-01-20 21:47:59 +0100

    fixed not all cards loading in on start up.

--- src/Components/CharacterInfo/Characterlist.js
+++ src/Components/CharacterInfo/Characterlist.js
@@ -1,58 +1,66 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import '../../App.css';
-import Card from "./Card";
-import {Jumbotron} from "../Jumbotron";
+import Card from './Card';
+import { Jumbotron } from '../Jumbotron';
 import axios from 'axios';
-import {render} from "@testing-library/react";
+import { render } from '@testing-library/react';
 
-
-export default class Characterlist extends Component{
-
-state={
+export default class Characterlist extends Component {
+  state = {
     url: 'https://rickandmortyapi.com/api/character/',
-    character: null
+    character: null,
+    search: '',
+  };
+
+  async componentDidMount() {
+    //make async function, what does it do? : this will run in the background,
+    // whilst other things are working.
+    const res = await axios.get(this.state.url); //request gives me response. await wait till its loaded then show characters
+    this.setState({ character: res.data['results'] }); //will re run render function
+  }
+
+  search = (event) => {
+    this.setState({ search: event.target.value });
+  };
+
+  render() {
+    return (
+      //sort of like a div but gets removed when it gets rendered so we dont have to make unecasrry divs
+      <React.Fragment>
+        <Jumbotron></Jumbotron>
+        {this.state.character ? (
+          <div className='container'>
+            <input
+              type='text'
+              onChange={this.search}
+              value={this.state.search}
+            />
+            <div className='row mt-5 justify-content-center'>
+              {this.state.character
+                .slice(0, 6)
+                .filter((character) =>
+                  character.name.includes(this.state.search)
+                )
+                .map((
+                  character //this.state.character ? () : () == if state is existing pass left and if it doesnt right
+                ) => (
+                  <Card
+                    name={character.name}
+                    id={character.id}
+                    key={character.id}
+                    image={character.image}
+                    status={character.status}
+                    species={character.species}
+                    gender={character.gender}
+                  />
+                ))}
+            </div>
+          </div>
+        ) : (
+          <h1 style={{ color: `#fafafa` }}>Loading Characters</h1>
+        )}
+        {/*if null return this ^*/}
+      </React.Fragment>
+    );
+  }
 }
-
-    async componentDidMount() { //make async function, what does it do? : this will run in the background,
-        // whilst other things are working.
-        const res = await axios.get(this.state.url) //request gives me response. await wait till its loaded then show characters
-        this.setState({character: res.data['results']}) //will re run render function
-    }
-
-    search = event => {
-        this.setState({ search: event.target.value })
-    }
-
-
-
-    render() {
-        return (
-//sort of like a div but gets removed when it gets rendered so we dont have to make unecasrry divs
-            <React.Fragment>
-                <Jumbotron></Jumbotron>
-                {this.state.character ? (
-                    <div className="container">
-                        <input type='text' onChange={this.search} value={this.state.search}/>
-                        <div className="row mt-5 justify-content-center">
-                        {this.state.character.slice(0,6).filter(character => character.name.includes(this.state.search)).map(character =>( //this.state.character ? () : () == if state is existing pass left and if it doesnt right
-                            <Card
-                                name={character.name}
-                                id={character.id}
-                                key={character.id}
-                                image={character.image}
-                                status={character.status}
-                                species={character.species}
-                                gender={character.gender}
-
-                            />
-                        ))}
-                        </div>
-                    </div> ) : (<h1 style={{color: `#fafafa`}}>Loading Characters</h1>) }
-                            {/*if null return this ^*/}
-            </React.Fragment>
-        );
-    }
-}
-
-
-
