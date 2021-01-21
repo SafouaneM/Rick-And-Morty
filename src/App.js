137171cd7f54be6c553516994c8cef6e32507fe4
Stijn Engelmoer <engelmoerstijn@gmail.com>
2021-01-21 21:10:27 +0100

    conflict fixen

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css';
 import NavBar from "./Components/layout/NavBar";
 import Dashboard from "./Components/layout/Dashboard";
-import backgroundimage  from './portalPattern.jpg'
 import SingleCharacter from "./Components/CharacterInfo/SingleCharacter";
 import {HashRouter as Router, Route, Switch } from "react-router-dom";
 import Locations from "./Components/Locations/Locations";
